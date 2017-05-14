0fbda4d44335c840eae3d4f62b59696841ae3615
Samuel <rott.samuel@gmail.com>
2017-05-14 12:43:36 +0200

    create balcony component

---
+++ src/components/garden/Balcony.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+import { Link } from 'react-router';
+
+
+export default function Terasse( props )
+{
+    return (
+
+        <div className="Container">
+					
+					<div className="Direction"><h1>mainzer strasse</h1></div>
+        </div>
+    );
+}
